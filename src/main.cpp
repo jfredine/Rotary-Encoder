2eba5cffceab65ee4d4711559232f7ad17591b5b
John Fredine <jfredine@hotmail.com>
2019-02-08 18:40:44 -0800

    Check both edges and clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
+/*
+ * Demonstrate reading a rotary encoder
+ *
+ * Author: John Fredine
+ * Original Date: 1/8/2019
+ */
+
 #include <Arduino.h>
 
-// Rotary encoder
+//******************************************************************************
+// User adjustable parameters
+// Rotary encoder pin connections
 // BTN_ENA leads BTN_ENB w/ clockwise rotation
-// BTN_ENC is low when button is pressed
 const int BTN_ENA = 8, BTN_ENB = 9;
 
-volatile int pos;
+// number of "stopping points" in a full revolution of the encoder
+const int num_detents_per_revolution = 20;
+
+// number of samples with constant signal value to be considered "debounced"
 const int debounce_stable_count = 4;
-#define OCR0A_INCR 64
+
+// time between interrupts for monitoring signal value
+// resolution of the timer is 4us so beset to make this a multiple of 4
+#define ISR_INTERVAL_us 256
+
+//******************************************************************************
+// calculated parameters which should not need user adjustment
+
+// assume a rising and folling edge of each button happens between detents
+const int num_edges_per_revolution = 2 * num_detents_per_revolution;
+
+// Number of timer ticks between interrupts.  Each tick is 4us
+#define OCR0A_INCR (ISR_INTERVAL_us / 4)
+
+// current position of the encoder
+volatile int pos;
 
 
 /*
  * TIMER0_COMPA_vect
  * ISR triggered by timer0 where inputs are read and debounced.
+ * In order to get interrupts at a rate greater than 1 ms (defaultfor timer0),
+ * but not require an additional timer and not disturbe the system timer, it
+ * makes use of the timer compare interrupt and adjusts the comparison value
+ * within the ISR.
  */
 
 ISR(TIMER0_COMPA_vect) {
@@ -25,7 +55,7 @@ ISR(TIMER0_COMPA_vect) {
     OCR0A = (OCR0A + OCR0A_INCR) & 0xff;
 
     // Check the value of button B when a rising edge on button A is found.
-    //  A rising edge on A when B is low indicates clockwise motion while a
+    // A rising edge on A when B is low indicates clockwise motion while a
     // rising edge on A when B is high indicates counter-clockwise motion
 
     // get value of B the first time we detect a change on A
@@ -40,29 +70,30 @@ ISR(TIMER0_COMPA_vect) {
         curr_a = val;
         curr_a_stable_count = 1;
     } else {
-        if ((curr_a_stable_count > 0) &&
-            (curr_a_stable_count < debounce_stable_count)) {
+        if (curr_a_stable_count > 0) {
             curr_a_stable_count++;
         }
     }
 
-    // if values are stable and rising edge on A, update position
+    // if values are stable, adjust rotary encoder position
     if (curr_a_stable_count == debounce_stable_count) {
-        if ((last_a == 0) && (curr_a == 1)) {
-            if (initial_b == 0) {
-                pos++;
-            } else {
+        if (last_a != curr_a) {
+            if (curr_a == initial_b) {
                 pos--;
+                if (pos < 0) {
+                    pos += num_edges_per_revolution;
+                }
+            } else {
+                pos++;
+                if (pos >= num_edges_per_revolution) {
+                    pos -= num_edges_per_revolution;
+                }
             }
-            if (pos < 0) {
-                pos += 20;
-            } else if (pos > 19) {
-                pos -= 20;
-            }
+
+            last_a = curr_a;
         }
 
         curr_a_stable_count = 0;
-        last_a = curr_a;
     }
 }
 
@@ -77,26 +108,27 @@ void setup() {
     // set up a recuring timer interrupt
     cli();  // disable interrupts
 
-    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));  // Turn off PWM modes
+    // Turn off PWM modes of timer0 because such modes prevent the
+    // immediate re-assignment of compare values
+    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));
     TCCR0B &= ~(1 << WGM02);
 
-    OCR0A = 1;              // set timer0/A to generate output when it
-                            // hits this value.  The ISR will adjust this
-                            // value to to potentially produce multiple
-                            // interrupts during the normal 0 -> FF count
-                            // sequence which takes 1024us
-    TIMSK0 |= _BV(OCIE0A);  // Set the compare A output to cause an interrupt
+    OCR0A = 1;              // Set the timer0 compare value.
+                            // The ISR will adjust this value to to potentially
+                            // produce multiple interrupts during the normal
+                            // 0 -> FF count sequence which takes 1024us
+    TIMSK0 |= _BV(OCIE0A);  // Enable the timer compare interrupt
 
     sei();  // enable interrupts
 }
+
 
 void loop() {
     static int last_pos;
 
     if (pos != last_pos) {
         Serial.print("Pos: ");
-        Serial.print(pos);
-        Serial.print("\n");
+        Serial.println(pos);
         last_pos = pos;
     }
 }
